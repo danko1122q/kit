eed1ba0c28ddd1848804a43156b709d7379d397a
danko12 <davaniko1122@gmail.com>
2025-11-05 00:00:22 +0700

    Update macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -2,6 +2,6 @@
 macro_rules! bat_warning {
     ($($arg:tt)*) => ({
         use nu_ansi_term::Color::Yellow;
-        eprintln!("{}: {}", Yellow.paint("[bat warning]"), format!($($arg)*));
+        eprintln!("{}: {}", Yellow.paint("[kit warning]"), format!($($arg)*));
     })
 }
